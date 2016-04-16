04534a10e5d781ee99af705467c538b55ec4db80
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-16 16:31:46 -0700

    functions to draw a single ts

--- vis/setup.js
+++ vis/setup.js
@@ -21,3 +21,9 @@ link_data = get_link_data(tree_cluster["links"], cur_abund); // displaying abund
 node_data = insert_node_abund(tree_cluster["nodes"], cur_abund);
 draw_links(svg_elem, link_data, scales);
 draw_nodes(svg_elem, tree_cluster["nodes"], scales);
+
+//one_ts = abund_ts["10032"]["16054"]
+//bounds = {"x_left": 10, "x_right": 50,
+//	  "y_top": 50, "y_bottom": 10}
+//ts_pos = get_one_ts_pos(bounds, one_ts)
+//draw_ts(svg_elem, [ts_pos])
---
+++ vis/ts_funs.js
@@ -0,0 +1,37 @@
+
+function get_one_ts_pos(bounds, one_ts) {
+  parse_date = d3.time.format("%Y-%m-%d").parse;
+  for (var i = 0; i < one_ts.length; i++) {
+    one_ts[i].time = parse_date(one_ts[i].time);
+  }
+  
+  // to get the extent
+  ts_x = one_ts.map(function(z) { return z.time });
+  ts_y = one_ts.map(function(z) { return z.value });
+
+  scales = {"x": d3.scale.linear()
+	    .domain(d3.extent(ts_x))
+	    .range([bounds["x_left"], bounds["x_right"]]),
+	    "y": d3.scale.linear()
+	    .domain(d3.extent(ts_y))
+	    .range([bounds.y_top, bounds.y_bottom])};
+
+  ts_pos = [];
+  for (var i = 0; i < one_ts.length; i++) {
+    ts_pos.push({"x": scales.x(one_ts[i].time),
+		 "y": scales.y(one_ts[i].value)});
+  }
+  return ts_pos;
+}
+
+function draw_ts(svg_elem, ts_pos) {
+  var line_fun = d3.svg.line()
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y; });
+
+  svg_elem.selectAll(".tsPath")
+    .data(ts_pos).enter()
+    .append("path")
+    .classed("tsPath", true)
+    .attr({"d": line_fun});
+}
