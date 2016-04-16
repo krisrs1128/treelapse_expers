636f87421620a0e03aa4efa02bd7b92f0112dbf2
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-16 14:14:47 -0700

    move function calls to separate setup file

---
+++ vis/setup.js
@@ -0,0 +1,22 @@
+width = 500;
+height = 250;
+
+tree_cluster = get_node_cluster(tree, width, height);
+
+svg_elem = d3.select("body")
+  .append("svg")
+  .attr({"width": width,
+	 "height": height});
+
+paddings = {"y_top": 25, "y_bottom": 25, "x_right": 25, "x_left": 25};
+scales = get_scales(height, width, paddings);
+
+times = Object.keys(abund);
+cur_abund = abund[times[0]]
+for (var otu_id in cur_abund) {
+  cur_abund[otu_id] = cur_abund[otu_id][0]
+}
+
+link_data = get_link_data(tree_cluster["links"], cur_abund); // displaying abundance at time 0
+draw_links(svg_elem, link_data, scales);
+draw_nodes(svg_elem, tree_cluster["nodes"], scales);
