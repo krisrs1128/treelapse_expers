ea399497ef601cc44a18f7514eb77145039a4e1b
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-17 14:14:20 -0700

    stylistic changes

--- vis/setup.js
+++ vis/setup.js
@@ -12,15 +12,15 @@ paddings = {"y_top": 25, "y_bottom": 25, "x_right": 25, "x_left": 25};
 scales = get_scales(height, width, paddings);
 
 times = Object.keys(abund);
-cur_abund = abund[times[0]]
+cur_abund = abund[times[0]];
 for (var otu_id in cur_abund) {
-  cur_abund[otu_id] = cur_abund[otu_id][0]
+  cur_abund[otu_id] = cur_abund[otu_id][0];
 }
 
-link_data = get_link_data(tree_cluster["links"], cur_abund); // displaying abundance at time 0
-node_data = insert_node_abund(tree_cluster["nodes"], cur_abund);
+link_data = insert_link_abund(tree_cluster.links, cur_abund);
+node_data = insert_node_abund(tree_cluster.nodes, cur_abund);
 draw_links(svg_elem, link_data, scales);
-draw_nodes(svg_elem, tree_cluster["nodes"], scales);
+draw_nodes(svg_elem, tree_cluster.nodes, scales);
 
 //one_ts = abund_ts["10032"]["16054"]
 //bounds = {"x_left": 10, "x_right": 50,
--- vis/ts_funs.js
+++ vis/ts_funs.js
@@ -6,12 +6,12 @@ function get_one_ts_pos(bounds, one_ts) {
   }
   
   // to get the extent
-  ts_x = one_ts.map(function(z) { return z.time });
-  ts_y = one_ts.map(function(z) { return z.value });
+  ts_x = one_ts.map(function(z) { return z.time; });
+  ts_y = one_ts.map(function(z) { return z.value; });
 
   scales = {"x": d3.scale.linear()
 	    .domain(d3.extent(ts_x))
-	    .range([bounds["x_left"], bounds["x_right"]]),
+	    .range([bounds.x_left, bounds.x_right]),
 	    "y": d3.scale.linear()
 	    .domain(d3.extent(ts_y))
 	    .range([bounds.y_top, bounds.y_bottom])};
