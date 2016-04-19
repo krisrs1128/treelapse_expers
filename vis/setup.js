b33df7917163732c0146319fff06164c06caa362
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-18 22:41:02 -0700

    try to reduce number of globals

--- vis/setup.js
+++ vis/setup.js
@@ -1,49 +1,37 @@
 width = 200;
-height = 900;
+height = 400;
 
-abund["10032"] = parse_times(abund["10032"]);
-
+// only worry about one person right now
+abund = parse_times(abund["10032"]);
 tree_cluster = get_node_cluster(tree, width, height);
-
 svg_elem = d3.select("body")
   .append("svg")
   .attr({"width": width,
 	 "height": height});
+svg_elem.append("rect")
+  .attr({"width": width,
+	 "height": height,
+	 "fill": "#F7F7F7"});
 
-paddings = {"y_top": 10, "y_bottom": 10, "x_right": 20, "x_left": 20};
-
+// just to get scales 
+paddings = {"y_top": 10, "y_bottom": 10, "x_right": 10, "x_left": 20};
 vis_extent = {"height": height, "width": 0.75 * width};
-
-cur_abund = {};
 abund_array = [];
-for (var key in abund["10032"]) {
-  cur_abund[key] = abund["10032"][key][0].value; // first timepoint
-  abund_array.push(cur_abund[key]);
+for (var key in abund) {
+  for (var i = 0; i < abund[key].length; i++) {
+    abund_array.push(abund[key][i].value);
+  }
 }
 
 x_values = tree_cluster.nodes.map(function(d) { return d.x; });
 y_values = tree_cluster.nodes.map(function(d) { return d.y; });
-data_extent = {"x": d3.max(y_values), "y": d3.max(x_values),
-	       "r": d3.max(abund_array)};
-
+data_extent = {"x": d3.max(y_values), "y": d3.max(x_values), "r": 5271};
 scales = get_scales(data_extent, vis_extent, paddings);
 
-
-link_data = insert_link_abund(tree_cluster.links, cur_abund);
-node_data = insert_node_abund(tree_cluster.nodes, cur_abund);
-draw_links(svg_elem, link_data, scales);
-draw_nodes(svg_elem, node_data, scales);
-
-draw_time_i = function(i) {
-  cur_abund = {};
-  for (var key in abund["10032"]) {
-    cur_abund[key] = abund["10032"][key][i].value; // 4th timepoint
-  }
-
-  node_data = insert_node_abund(tree_cluster.nodes, cur_abund);
-  draw_nodes(svg_elem, node_data, scales);
-}
-
+// draw the ts
 tips = get_tips(tree_cluster.nodes);
 bounds = get_ts_bounds(tips, scales, 0.75 * width, width);
-draw_tip_ts(svg_elem, abund["10032"], tips, bounds);
+draw_tip_ts(svg_elem, abund, tips, bounds);
+ts_extents = get_ts_extent(abund);
+draw_ts_brush(svg_elem, ts_extents, bounds[0], scales);
+draw_phylo(svg_elem, abund, ts_extents.time, tree_cluster, scales);
