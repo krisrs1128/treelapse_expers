fff968385b0ebd79f50b79aece6ed77a48d86550
Kris Sankaran <sankaran.kris@gmail.com>
2016-05-19 20:55:05 -0700

    update drawing experiment to display tree segmentation

--- doi/doi_draw.js
+++ doi/doi_draw.js
@@ -4,18 +4,24 @@
 //////////////////////////////////////////////////////////////////////
 
 var tree = d3.layout.tree()
-var nodes = tree.nodeSize([5, 15])
-    .nodes(tax_tree) // this actually modifies the tax tree variable
+var nodes = tree.nodeSize([4, 10])
+    .nodes(tax_tree); // this actually modifies the tax tree variable
+
+tax_tree2 = segment_tree(tax_tree);
+tax_tree2 = tax_tree;
+nodes = tree.nodeSize([4, 50])
+  .nodes(tax_tree2);
     
 var nodes_pos = {"x": nodes.map(function(d) { return d.x }),
 		 "y": nodes.map(function(d) { return d.y })}
 
 var links = tree.links(nodes)
+
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.x, d.y]})
 
-var height = 500,
-    width = 500
+var height = 800,
+    width = 1000
 
 // just draw it for fun
 var svg_elem = d3.select("body")
@@ -34,6 +40,10 @@ d3.select("svg")
     return diagonal({"source": source, "target": target})
   }})
 
+var colScale = d3.scale.ordinal()
+    .domain(d3.range(12))
+    .range(["#F88E79","#FA8B8C","#F58CA0","#EA90B3","#D997C4","#C29FD0","#A6A7D8","#87AFD9","#67B5D4","#4BBAC8","#3ABDB8","#3EBFA3"])
+
 d3.select("svg")
   .selectAll(".tree_node")
   .data(nodes).enter()
@@ -42,4 +52,6 @@ d3.select("svg")
   .attr({"cx": function(d) { return d.x },
 	 "cy": function(d) { return d.y},
 	 "r": 2,
-	 "fill": "#A4A7DA"})
+	 "fill": function(d) { return colScale(d.segment) }});
+
+test = segment_tree(tax_tree)
