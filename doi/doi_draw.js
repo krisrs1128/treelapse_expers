8bd03792eb6c99ef6351c5858105a4b551c4912a
Kris Sankaran <sankaran.kris@gmail.com>
2016-05-21 22:17:19 -0700

    similar issues with other parameters, just make them all global

--- doi/doi_draw.js
+++ doi/doi_draw.js
@@ -7,6 +7,7 @@ var height = 300,
     width = 500
 var node_size = [10, 50]
 var min_doi = -3.5
+var tree_var = tax_tree
 
 // just draw it for fun
 var svg_elem = d3.select("body")
@@ -25,20 +26,20 @@ var display_dim = [width, height];
 d3.select("#min_doi")
   .on("input", function() {
     min_doi = this.value
-    doi_update(tax_tree, display_dim, node_size);
+    doi_update();
   });
 
 d3.select("#node_x")
   .on("input", function() {
     node_size[0] = this.value
-    doi_update(tax_tree, display_dim, node_size);
+    doi_update();
   });
 
 d3.select("#node_y")
   .on("input", function() {
     node_size[1] = this.value
-    doi_update(tax_tree, display_dim, node_size);
+    doi_update();
   });
 
 
-doi_update(tax_tree, display_dim, node_size);
+doi_update();
