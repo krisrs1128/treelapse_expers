72aac00a397370a736629b59195774554454f4a9
Kris Sankaran <sankaran.kris@gmail.com>
2016-05-21 17:50:09 -0700

    use doi updating

--- doi/doi_draw.js
+++ doi/doi_draw.js
@@ -3,56 +3,42 @@
 // are following http://vis.stanford.edu/papers/doitrees-revisited
 //////////////////////////////////////////////////////////////////////
 
-var height = 500,
+var height = 300,
     width = 500
-var node_size = [4, 40]
-
-var nodes = d3.layout.cluster()
-    .nodeSize(node_size)
-    .nodes(tax_tree); // this actually modifies the tax tree variable
-
-var res = tree_block(tax_tree, "F:F:Micrococcaceae", -10,
-		     [width, height], node_size);
-nodes = res.nodes;
-
-var x_extent = d3.extent(nodes.map(function(d) { return d.x }));
-for (var i = 0; i < nodes.length; i++) {
-  nodes[i].x += (x_extent[1] - x_extent[0]) / 2
-  nodes[i].y = nodes[i].depth * node_size[1];
-}
-
-var links = d3.layout.cluster()
-    .links(nodes)
-var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.x, d.y]})
+var node_size = [10, 50]
+var min_doi = -3.5
 
 // just draw it for fun
 var svg_elem = d3.select("body")
   .append("svg")
   .attr({"width": width,
 	 "height": height})
-
 d3.select("svg")
-  .selectAll(".tree_link")
-  .data(links).enter()
-  .append("path", "g")
-  .classed("tree_link", true)
-  .attr({"d": function(d) {
-    var source = {"x": d.source.x, "y": d.source.y}
-    var target = {"x": d.target.x, "y": d.target.y}
-    return diagonal({"source": source, "target": target})
-  }})
+  .append("rect")
+  .attr({"width": width,
+	 "height": height,
+	 "fill": "#F5F5F5"})
 
-var colScale = d3.scale.ordinal()
-    .domain([-10, 0])
-    .range(["#F88E79","#FA8B8C","#F58CA0","#EA90B3","#D997C4","#C29FD0","#A6A7D8","#87AFD9","#67B5D4","#4BBAC8","#3ABDB8","#3EBFA3"])
+var focus_node_id = "K:Bacteria"
+var display_dim = [width, height];
 
-d3.select("svg")
-  .selectAll(".tree_node")
-  .data(nodes).enter()
-  .append("circle")
-  .classed("tree_node", true)
-  .attr({"cx": function(d) { return d.x },
-	 "cy": function(d) { return d.y},
-	 "r": 2,
-	 "fill": function(d) { return colScale(d.doi) }});
+d3.select("#min_doi")
+  .on("input", function() {
+    min_doi = this.value
+    doi_update(tax_tree, min_doi, display_dim, node_size);
+  });
+
+d3.select("#node_x")
+  .on("input", function() {
+    node_size[0] = this.value
+    doi_update(tax_tree, min_doi, display_dim, node_size);
+  });
+
+d3.select("#node_y")
+  .on("input", function() {
+    node_size[1] = this.value
+    doi_update(tax_tree, min_doi, display_dim, node_size);
+  });
+
+
+doi_update(tax_tree, min_doi, display_dim, node_size);
