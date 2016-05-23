23cae068b6f154727b6b3158b723a198b30c07a5
Kris Sankaran <sankaran.kris@gmail.com>
2016-05-23 16:54:55 -0700

    start annotating doi tree, with tax labels for >= -1 doi nodes

---
+++ doi_supp/doi_draw.js
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////////
+// Functions to calculate degree-of-interest related quantities. We
+// are following http://vis.stanford.edu/papers/doitrees-revisited
+//////////////////////////////////////////////////////////////////////
+
+var height = 300,
+    width = 500
+var node_size = [10, 50]
+var min_doi = -3.5
+var tree_var = tax_tree
+
+// just draw it for fun
+var svg_elem = d3.select("body")
+  .append("svg")
+  .attr({"width": width,
+	 "height": height})
+d3.select("svg")
+  .append("rect")
+  .attr({"width": width,
+	 "height": height,
+	 "fill": "#F5F5F5"})
+
+d3.select("svg")
+  .append("g")
+  .attr({"id": "links"});
+d3.select("svg")
+  .append("g")
+  .attr({"id": "text"});
+d3.select("svg")
+  .append("g")
+  .attr({"id": "nodes"});
+
+var focus_node_id = "Bacteria"
+var display_dim = [width, height];
+
+d3.select("#min_doi")
+  .on("input", function() {
+    min_doi = this.value
+    doi_update();
+  });
+
+d3.select("#node_x")
+  .on("input", function() {
+    node_size[0] = this.value
+    doi_update();
+  });
+
+d3.select("#node_y")
+  .on("input", function() {
+    node_size[1] = this.value
+    doi_update();
+  });
+
+
+doi_update();
---
+++ doi_supp/doi_update.js
@@ -0,0 +1,103 @@
+
+function doi_update() {
+  var layout = tree_block(tree_var, focus_node_id, min_doi,
+			  display_dim, node_size);
+  
+  var links = d3.layout.cluster()
+    .links(layout.nodes)
+  var diagonal = d3.svg.diagonal()
+      .projection(function(d) { return [d.x, d.y]})
+  
+  var link_selection = d3.select("#links")
+      .selectAll(".tree_link")
+      .data(links, function(d) {
+	return d.source.name + "-" + d.target.name
+      })
+
+  var node_selection = d3.select("#nodes")
+      .selectAll(".tree_node")
+      .data(layout.nodes, function(d) { return d.name })
+
+  var text_selection = d3.select("#text")
+      .selectAll(".tree_text")
+      .data(layout.nodes.filter(function(d) { return d.doi >= -1}),
+	    function(d) { return d.name });
+
+  link_selection.exit().remove();
+  node_selection.exit().remove();
+  text_selection.exit().remove();
+
+  // enter links and nodes that haven't been entered yet
+  link_selection.enter()
+    .append("path", "g")
+    .classed("tree_link", true)
+    .style({"opacity": 0})
+
+  var col_scale = d3.scale.linear()
+      .domain([-7, 0])
+      .range(["#F88E79", "#3EBFA3"])
+
+  node_selection.enter()
+    .append("circle")
+    .classed("tree_node", true)
+    .style({"opacity": 0,
+	    "fill": function(d) {
+	      return col_scale(d.doi) }})
+    .on("click",
+	function(d) {
+	  focus_node_id = d.name;
+	  doi_update();
+	});
+
+  text_selection.enter()
+    .append("text")
+    .classed("tree_text", true)
+    .style({"opacity": 0});
+
+  link_selection
+    .transition()
+    .duration(1000)
+    .attr({"d": function(d) {
+      var source = {"x": d.source.x, "y": d.source.y}
+      var target = {"x": d.target.x, "y": d.target.y}
+      return diagonal({"source": source, "target": target})
+    }})
+    .style({"opacity": 1})
+  
+  node_selection
+    .transition()
+    .duration(1000)
+    .attr({"cx": function(d) { return d.x },
+	   "cy": function(d) { return d.y},
+	   "r": 2})
+    .style({"opacity": 1,
+	    "stroke": "red",
+	    "stroke-width": function(d) {
+	      if(d.doi == 0) {
+		return .5;
+	      } else {
+		return 0;
+	      }},
+	    "fill": function(d) {
+	      return col_scale(d.doi) }});
+
+  text_selection
+    .transition()
+    .duration(1000)
+    .text(function(d) { return d.name })
+    .attr({"x": function(d) { return 5 + d.x; },
+	   "y": function(d) { return d.y; },
+	   "font-size": function(d) {
+	     if(d.doi == 0) {
+	       return 8
+	     } else {
+	       return 5
+	     }}})
+    .style({"opacity":  function(d) {
+	     if(d.doi == 0) {
+	       return 1
+	     } else {
+	       return .6
+	     }}});
+
+}
