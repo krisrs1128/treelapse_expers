3b53b3099a6ec0e7d2b7e6a72febdddb2c592166
Kris Sankaran <sankaran.kris@gmail.com>
2016-06-01 19:59:41 -0700

    start an example with interactive comparisons

---
+++ doi_compare/doi_draw.js
@@ -0,0 +1,79 @@
+//////////////////////////////////////////////////////////////////////
+// Functions to calculate degree-of-interest related quantities. We
+// are following http://vis.stanford.edu/papers/doitrees-revisited
+//////////////////////////////////////////////////////////////////////
+
+var height = 300,
+    width = 1000;
+var node_size = [$("#node_x").attr("value"), $("#node_y").attr("value")];
+var min_doi = $("#min_doi").attr("value");
+var min_avg_abund = $("#min_avg_abund").attr("value");
+var tree_var = jQuery.extend(true, {}, tax_tree);
+var abund_vars = tax_abund
+
+// just draw it for fun
+var svg_elem = d3.select("body")
+  .append("svg")
+  .attr({"width": width,
+	 "height": height})
+d3.select("svg")
+  .append("rect")
+  .attr({"width": width,
+	 "height": height,
+	 "fill": "#F7F7F7"})
+
+d3.select("svg")
+  .selectAll("g")
+  .data(["highlight-links", "links", "nodes", "text"]).enter()
+  .append("g")
+  .attr({"id": function(d) { return d; }});
+
+for (var i = 0; i < Object.keys(abund_vars).length; i++) {
+  d3.select("#nodes")
+    .append("g")
+    .attr({"id": "group-" + i})
+}
+
+
+var focus_node_id = "Bacteria";
+var highlight_ids = [];
+var display_dim = [width, height];
+
+d3.select("#min_doi")
+  .on("input", function() {
+    min_doi = this.value;
+    doi_update();
+  });
+
+d3.select("#min_avg_abund")
+  .on("input", function() {
+    min_avg_abund = this.value;
+    doi_update();
+  });
+
+d3.select("#node_x")
+  .on("input", function() {
+    node_size[0] = this.value
+    doi_update();
+  });
+
+d3.select("#node_y")
+  .on("input", function() {
+    node_size[1] = this.value
+    doi_update();
+  });
+
+doi_update();
+
+$(document).ready(function() {
+  var timeoutID = null;
+  $('#search_box').keyup(function() {
+    clearTimeout(timeoutID);
+    var $target = $(this);
+    timeoutID = setTimeout(function() {
+      highlight_ids = get_ancestor_matches($target.val());
+      tree_var = jQuery.extend(true, {}, tax_tree);
+      doi_update();
+    }, 10);
+  })
+});
---
+++ doi_compare/doi_update.js
@@ -0,0 +1,92 @@
+
+function doi_update() {
+  var filtered_tree = filter_tree(jQuery.extend(true, {}, tree_var),
+				  min_avg_abund);
+  var layout = tree_block(filtered_tree, focus_node_id, min_doi,
+			  display_dim, node_size);
+  var group_ids = Object.keys(abund_vars);
+  
+  var tmp = [];
+  for (var group_id in abund_vars) {
+    for (var otu_id in abund_vars[group_id]) {
+      for (var time_id in abund_vars[group_id][otu_id]) {
+	tmp.push(abund_vars[group_id][otu_id][time_id].value);
+      }
+    }
+  }
+
+  var max_abund = d3.max(tmp);
+  var scales = {"size": d3.scale.linear()
+		.domain([0, max_abund])
+		.range([.7, 13]),
+	        "col": d3.scale.ordinal()
+		.domain(Object.keys(abund_vars))
+		.range(["#B1A1E6", "#F5916A"])};
+
+  var links = d3.layout.cluster()
+    .links(layout.nodes)
+  var diagonal = d3.svg.diagonal()
+      .projection(function(d) { return [d.x, d.y]})
+
+  var link_selection = d3.select("#links")
+      .selectAll(".tree_link")
+      .data(links, link_id_fun);
+
+  for (var j = 0; j < group_ids.length; j++) {
+    draw_nodes(d3.select("#nodes #group-" + j), layout.nodes,
+	       abund_vars[group_ids[j]], group_ids[j], scales);
+  }
+}
+
+function draw_nodes(el, nodes, abunds, group_id, scales) {
+  var node_selection = el.selectAll(".tree_nodes")
+      .data(nodes, function(d) { return group_id + "-" + d.name; });
+  node_selection.exit().remove();
+  node_selection.enter()
+    .append("circle")
+    .classed("tree_nodes", true)
+    .attr({"cx": function(d) { return d.x; },
+	   "cy": function(d) { return d.y; }})
+    .style({"opacity": 0,
+	    "r": function(d) { return scales.size(d3.mean(get_abunds(abunds, d.name)))},
+	    "fill": function(d) { return scales.col(group_id)}});
+
+  node_selection.transition()
+    .duration(700)
+    .attr({"cx": function(d) { return d.x; },
+	   "cy": function(d) { return d.y; },
+	   "r": function(d) { return scales.size(d3.mean(get_abunds(abunds, d.name)))}})
+    .style({"opacity": 0.5,
+	    "fill": function(d) { return scales.col(group_id)}});
+}
+
+function get_matches(names, search_str) {
+  var matches = [];
+  search_str = search_str.toLowerCase();
+  var lower_names = names.map(function(d) { return d.toLowerCase(); });
+
+  for (var i = 0; i < names.length; i++) {
+    if (lower_names[i].search(search_str) != -1) {
+      matches.push(names[i]);
+    }
+  }
+  return matches;
+}
+
+function get_ancestor_matches(search_str) {
+  if (search_str == "") return [];
+
+  var nodes = d3.layout.cluster()
+      .nodes(tree_var);
+  var names = nodes.map(function(d) { return d.name; });
+  var matches = get_matches(names, search_str);
+  
+  var ancestor_matches = [];
+  for (var i = 0; i < matches.length; i++) {
+    ancestor_matches = ancestor_matches.concat(
+      get_ancestors(tree_var, matches[i], [])
+    );
+    ancestor_matches = _.uniq(ancestor_matches);
+  }
+  return ancestor_matches;
+}
